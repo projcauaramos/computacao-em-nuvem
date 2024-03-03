94407577ae3f2cacd24fec812a4c6ebadf44b161
Cauã Ramos <125595788+projcauaramos@users.noreply.github.com>
2024-03-03 14:45:11 -0300

    Subi os arquivos do meu site

---
+++ script.js
@@ -0,0 +1,40 @@
+// scripts.js
+// Função para destacar o link do menu conforme a rolagem da página
+window.addEventListener('scroll', function() {
+    var sections = document.querySelectorAll('section');
+    var navLinks = document.querySelectorAll('nav ul li a');
+  
+    sections.forEach(function(section, index) {
+      var position = section.offsetTop;
+      var sectionHeight = section.offsetHeight;
+  
+      if (window.scrollY >= position && window.scrollY < position + sectionHeight) {
+        navLinks[index].classList.add('active');
+      } else {
+        navLinks[index].classList.remove('active');
+      }
+    });
+  });
+
+// Array de imagens
+const images = ['img/cloud1.jpg', 'img/cloud2.jpg', 'img/cloud3.jpg'];
+
+// Obtém a referência para a imagem
+const cloudImage = document.getElementById('cloudImage');
+
+// Variável para controlar o índice da imagem atual
+let currentIndex = 0;
+
+// Função para atualizar a imagem
+function changeImage() {
+  cloudImage.classList.add('hidden');
+  setTimeout(() => {
+    currentIndex = (currentIndex + 1) % images.length;
+    cloudImage.src = images[currentIndex];
+    cloudImage.classList.remove('hidden');
+  }, 500);
+}
+
+// Altera a imagem a cada 5 segundos
+setInterval(changeImage, 5000);
+
